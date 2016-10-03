a3982fa71590cb108912f5e1944bd5363e854cee
Giles Roadnight <giles@roadnight.name>
2016-10-03 11:13:23 +0100

    add template project files

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+var gulp = require('gulp'),
+    ts = require('gulp-typescript'),
+    clean = require('gulp-clean'),
+	processhtml = require('gulp-processhtml'),
+	uglify = require('gulp-uglify'),
+	fs = require('fs'),
+	replace = require('gulp-replace'),
+	debug = require('gulp-debug');
+
+var releaseFolder = "release";
+var packageJson = JSON.parse(fs.readFileSync('./package.json'));
+
+gulp.task('clean', function () {
+	return gulp.src(releaseFolder, {read: false})
+		.pipe(clean());
+});
+
+/**
+ * Compile TypeScript and include references to library and app .d.ts files.
+ */
+gulp.task('compile-ts', ['clean'], function () {
+	var tsProject = ts.createProject('tsconfig.json', {outFile: "app.js"});
+    var tsResult = tsProject.src().pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest("./"));
+});
+
+gulp.task('copy-release', ['clean'], function () {
+	return gulp.src([
+		'./templates/**/*.html',
+		'./assets/**/*.*',
+		'./css/**/*.css'
+	], {base: './'})
+  		.pipe(gulp.dest(releaseFolder));
+});
+
+gulp.task('process-html', ['clean'], function () {
+	return gulp.src("index.html")
+		   .pipe(processhtml())
+		   .pipe(gulp.dest(releaseFolder));
+});
+
+gulp.task('uglify', ['compile-ts'], function () {
+	return gulp.src("app.js")
+		   .pipe(uglify())
+		   .pipe(gulp.dest(releaseFolder));
+});
+
+gulp.task('replace', ['uglify','process-html','copy-release'], function () {
+	return gulp.src([releaseFolder + '/**/*.html',releaseFolder + '/app.js'])
+		   .pipe(replace( "__applicationVersionNumber__", packageJson.version ))
+		   .pipe(gulp.dest(releaseFolder));
+});
+
+gulp.task('default', ['clean','compile-ts','copy-release','process-html','uglify','replace']);
---
+++ ts/app.ts
@@ -0,0 +1,40 @@
+/**
+ * Created by Giles on 08/02/2016.
+ */
+
+/// <reference path="../node_modules/rx/ts/rx.all.d.ts"/>
+/// <reference path="../node_modules/rx-angular/ts/rx.angular.d.ts"/>
+/// <reference path="../typings/browser.d.ts"/>
+/// <reference path="Google.Services/youTubeService.ts"/>
+/// <reference path="Google.Services/googleAuthenticationService.ts"/>
+/// <reference path="Pricklythistle/services/commentsService.ts"/>
+/// <reference path="Pricklythistle/controller/commentatorController.ts"/>
+/// <reference path="Pricklythistle/controller/loginController.ts"/>
+/// <reference path="Pricklythistle/controller/headerController.ts"/>
+
+const scopes: string = "https://www.googleapis.com/auth/youtube https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/youtube.force-ssl";
+
+const app: ng.IModule = angular.module( "youTubeCommentator", [ "ngRoute", "ngCookies", "ngSanitize", "infinite-scroll", "monospaced.elastic" ] );
+
+app.controller( "commentatorController", Pricklythistle.Controller.CommentatorController )
+	.controller( "loginController", Pricklythistle.Controller.LoginController )
+	.controller( "headerController", Pricklythistle.Controller.HeaderController );
+
+app.service( "googleAuthenticationService", Google.Services.GoogleAuthenticationService )
+	.service( "youTubeService", Google.Services.YouTubeService )
+	.service( "commentService", Pricklythistle.Services.CommentService );
+
+app.config( ['$routeProvider',( $routeProvider: angular.route.IRouteProvider ) => {
+    $routeProvider
+        .when( "/login", { templateUrl:"templates/login.html?v=__applicationVersionNumber__", controller: "loginController as controller" } )
+        .when( "/comments", { templateUrl:"templates/comments.html?v=__applicationVersionNumber__", controller: "commentatorController as commentsController" } )
+        .otherwise( { redirectTo: "/login" } );
+}] );
+
+angular.module('infinite-scroll').value('THROTTLE_MILLISECONDS', 500);
+
+app.value( "authorizationScopes", scopes);
+
+function initiateApp() {
+    angular.bootstrap( document, ["youTubeCommentator"] );
+}
